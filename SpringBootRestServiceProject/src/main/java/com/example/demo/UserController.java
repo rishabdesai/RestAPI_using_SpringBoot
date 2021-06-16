01ad01cf2b303a899bc179221d7df9743d6028d7
Rishabh Desai <desai.rishab@gmail.com>
2021-06-16 21:55:59 +0530

    added more PUT requests

--- SpringBootRestServiceProject/src/main/java/com/example/demo/UserController.java
+++ SpringBootRestServiceProject/src/main/java/com/example/demo/UserController.java
@@ -2,36 +2,49 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	
+
+//	@GetMapping
+//	public String getUsers() {
+//		return "http get request was sent";
+//	}
+
 	@GetMapping
-	public String getUsers() {
-		
-		return "http get request was sent";
+	public String getUsers(@RequestParam(value="page") int pageNo, @RequestParam(value="limit") int limitNo) {
+		return "http get request was sent using query paramenter and value of page is  :"+pageNo+"   and limit no is :  "+limitNo;
 	}
 	
+	
+	@GetMapping(path = "/{userID}") // path parameter
+	public String getUser(@PathVariable String userID) {
+
+		return "http get request was sent for user ID   :	" + userID;
+	}
+
 	@PostMapping
 	public String createUser() {
-		
+
 		return "http POST request was sent";
 	}
-	
+
 	@PutMapping
 	public String updateUser() {
-		
+
 		return "http put request was sent";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
-		
+
 		return "http delete request was sent";
 	}
 
