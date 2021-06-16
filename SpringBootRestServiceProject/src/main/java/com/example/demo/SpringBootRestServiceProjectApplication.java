c228a28213f16c9b6f32b3c70bf1e9843d05e941
Rishabh Desai <desai.rishab@gmail.com>
2021-06-16 20:51:11 +0530

    Spring BootRestServiceProject added

---
+++ SpringBootRestServiceProject/src/main/java/com/example/demo/SpringBootRestServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServiceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiceProjectApplication.class, args);
+	}
+
+}
